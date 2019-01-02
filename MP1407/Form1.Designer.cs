73b69f60c7cad8e09cef5a72c5740933dc4ff681
CHENYANLIANG-duke <46186328+CHENYANLIANG-duke@users.noreply.github.com>
2019-01-02 17:58:44 +0800

    加入專案檔案。

---
+++ MP1407/Form1.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace MP1407
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btn_CloseReader = new System.Windows.Forms.Button();
+            this.btn_OpenReader = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtmsg = new System.Windows.Forms.TextBox();
+            this.btn_AutoReadUltraLight = new System.Windows.Forms.Button();
+            this.txtthreadtime = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.labcomchoice = new System.Windows.Forms.Label();
+            this.ComportName = new System.Windows.Forms.ComboBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_CloseReader
+            // 
+            this.btn_CloseReader.Enabled = false;
+            this.btn_CloseReader.Location = new System.Drawing.Point(110, 60);
+            this.btn_CloseReader.Name = "btn_CloseReader";
+            this.btn_CloseReader.Size = new System.Drawing.Size(86, 29);
+            this.btn_CloseReader.TabIndex = 94;
+            this.btn_CloseReader.Text = "Close Reader";
+            this.btn_CloseReader.UseVisualStyleBackColor = true;
+            this.btn_CloseReader.SizeChanged += new System.EventHandler(this.btn_CloseReader_SizeChanged);
+            this.btn_CloseReader.Click += new System.EventHandler(this.btn_CloseReader_Click);
+            // 
+            // btn_OpenReader
+            // 
+            this.btn_OpenReader.Location = new System.Drawing.Point(18, 60);
+            this.btn_OpenReader.Name = "btn_OpenReader";
+            this.btn_OpenReader.Size = new System.Drawing.Size(86, 29);
+            this.btn_OpenReader.TabIndex = 96;
+            this.btn_OpenReader.Text = "Open Reader";
+            this.btn_OpenReader.UseVisualStyleBackColor = true;
+            this.btn_OpenReader.Click += new System.EventHandler(this.btn_OpenReader_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtmsg);
+            this.groupBox1.Location = new System.Drawing.Point(12, 109);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(424, 157);
+            this.groupBox1.TabIndex = 97;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "NDEF Message";
+            // 
+            // txtmsg
+            // 
+            this.txtmsg.Location = new System.Drawing.Point(6, 21);
+            this.txtmsg.Multiline = true;
+            this.txtmsg.Name = "txtmsg";
+            this.txtmsg.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtmsg.Size = new System.Drawing.Size(412, 129);
+            this.txtmsg.TabIndex = 97;
+            // 
+            // btn_AutoReadUltraLight
+            // 
+            this.btn_AutoReadUltraLight.Enabled = false;
+            this.btn_AutoReadUltraLight.Location = new System.Drawing.Point(202, 60);
+            this.btn_AutoReadUltraLight.Name = "btn_AutoReadUltraLight";
+            this.btn_AutoReadUltraLight.Size = new System.Drawing.Size(120, 30);
+            this.btn_AutoReadUltraLight.TabIndex = 94;
+            this.btn_AutoReadUltraLight.Text = "AutoRead UltraLight";
+            this.btn_AutoReadUltraLight.UseVisualStyleBackColor = true;
+            this.btn_AutoReadUltraLight.Click += new System.EventHandler(this.btn_AutoReadUltraLight_Click);
+            // 
+            // txtthreadtime
+            // 
+            this.txtthreadtime.AcceptsReturn = true;
+            this.txtthreadtime.BackColor = System.Drawing.SystemColors.Window;
+            this.txtthreadtime.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtthreadtime.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtthreadtime.Font = new System.Drawing.Font("細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtthreadtime.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.txtthreadtime.Location = new System.Drawing.Point(338, 44);
+            this.txtthreadtime.MaxLength = 8;
+            this.txtthreadtime.Name = "txtthreadtime";
+            this.txtthreadtime.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.txtthreadtime.Size = new System.Drawing.Size(65, 22);
+            this.txtthreadtime.TabIndex = 100;
+            this.txtthreadtime.Text = "500";
+            this.txtthreadtime.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label8.Font = new System.Drawing.Font("細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label8.Location = new System.Drawing.Point(338, 24);
+            this.label8.Name = "label8";
+            this.label8.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.label8.Size = new System.Drawing.Size(67, 17);
+            this.label8.TabIndex = 101;
+            this.label8.Text = "Thread(ms)";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "2D Scaner";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // labcomchoice
+            // 
+            this.labcomchoice.AutoSize = true;
+            this.labcomchoice.Font = new System.Drawing.Font("標楷體", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.labcomchoice.Location = new System.Drawing.Point(15, 22);
+            this.labcomchoice.Name = "labcomchoice";
+            this.labcomchoice.Size = new System.Drawing.Size(79, 15);
+            this.labcomchoice.TabIndex = 103;
+            this.labcomchoice.Text = "選擇COM :";
+            // 
+            // ComportName
+            // 
+            this.ComportName.FormattingEnabled = true;
+            this.ComportName.Location = new System.Drawing.Point(100, 21);
+            this.ComportName.Name = "ComportName";
+            this.ComportName.Size = new System.Drawing.Size(79, 20);
+            this.ComportName.TabIndex = 102;
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.PortName = "COM4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 281);
+            this.Controls.Add(this.labcomchoice);
+            this.Controls.Add(this.ComportName);
+            this.Controls.Add(this.txtthreadtime);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btn_OpenReader);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.btn_CloseReader);
+            this.Controls.Add(this.btn_AutoReadUltraLight);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Unitech_MP1407";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        internal System.Windows.Forms.Button btn_CloseReader;
+        internal System.Windows.Forms.Button btn_OpenReader;
+        private System.Windows.Forms.GroupBox groupBox1;
+        internal System.Windows.Forms.Button btn_AutoReadUltraLight;
+        public System.Windows.Forms.TextBox txtthreadtime;
+        public System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtmsg;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label labcomchoice;
+        private System.Windows.Forms.ComboBox ComportName;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ MP1407/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Text.RegularExpressions;
+using System.Collections;
+using System.Runtime.InteropServices;
+using System.IO.Ports;
+
+namespace MP1407
+{
+    public partial class Form1 : Form
+    {
+        int M_iResult;
+        List<byte> devcomport = new List<byte>();
+        List<byte> devcomport2 = new List<byte>();
+        string[] ports = SerialPort.GetPortNames();
+        private byte[] Key = new byte[(int)RDINT.MIFARE_ONE_DEF.CARD_KEY_LEN];
+      
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+            foreach (string port in ports)
+            {
+                ComportName.Items.Add(port);
+            }
+            try
+            {
+                ComportName.SelectedIndex = 0;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("No detected COM !");
+            }
+        }
+
+        private void btn_OpenReader_Click(object sender, EventArgs e)
+        {
+            uint intBar = 0;
+            txtmsg.Clear();
+
+            var com = Convert.ToInt32(ComportName.SelectedItem.ToString().Remove(0, 3));
+            M_iResult = (int)RDINT.RDINTsys.OpenReader((byte)com, uint.Parse("38400"), "00000000", RDINT.TURN_ON_OFF.TURN_ON, uint.Parse("0"), out intBar);
+
+            if (!(M_iResult == 0))
+            {
+                ShowStatus("Open Reader" + com.ToString() + " 失敗。錯誤代碼:" + M_iResult);
+            }
+            else
+            {
+                ShowStatus("Open Reader  com" + com.ToString() + " 成功");
+                devcomport2.Add((byte)com);
+            }
+
+            btn_OpenReader.Enabled = false;
+            btn_CloseReader.Enabled = true;
+            btn_AutoReadUltraLight.Enabled = true;
+
+        }
+
+        private void btn_CloseReader_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+          
+            txtmsg.Clear();
+
+            for (int i = 0; i < devcomport2.Count; i++)
+            {
+                M_iResult = (int)RDINT.RDINTsys.CloseReader(devcomport2[i]);
+
+
+                if (M_iResult == 0)
+                {
+                    ShowStatus("Close Reader" + (i + 1).ToString() + "成功");
+                }
+                else
+                    ShowStatus("Close Reader" + (i + 1).ToString() + " 失敗。錯誤代碼:");
+            }
+
+            btn_CloseReader.Enabled = false;
+            btn_OpenReader.Enabled = true;
+            btn_AutoReadUltraLight.Enabled = true;
+            txtthreadtime.Enabled = true;
+        }
+
+        private void btn_AutoReadUltraLight_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = Convert.ToInt32(txtthreadtime.Text);
+            timer1.Start();
+            txtthreadtime.Enabled = false;
+            btn_AutoReadUltraLight.Enabled = false;
+        }
+
+        private void ShowStatus(String Msg)
+        {
+            txtmsg.Text += Msg + "\r\n";
+        }
+
+        private void readnfc2(int comNo)
+        {
+            txtmsg.Clear();
+  
+            try
+            {
+                //GET UID
+                int i, j;
+                byte Sak;
+                byte[] Uid = new byte[(int)RDINT.OPENCARD_DEF.UID_LEN];
+                byte[] Atqa = new byte[(int)RDINT.OPENCARD_DEF.ATQA_LEN];
+                string strUID = "";
+
+                //u1AutoFind設定TURN_ON，卡片關閉後必須重新靠卡，才可開啟卡片
+                M_iResult = (int)RDINT.RDINTnon.OpenCard(devcomport2[comNo], (RDINT.TURN_ON_OFF)RDINT.TURN_ON_OFF.TURN_ON, Uid, Atqa, out Sak);
+                if (M_iResult != 0)
+                {
+                    // ShowStatus("Open Card失敗。錯誤代碼:" + M_iResult);
+                    return;
+                }
+
+                for (i = (int)RDINT.OPENCARD_DEF.UID_LEN - 1; i >= 0; i -= 1)
+                {
+                    if (Uid[i] == (int)RDINT.OPENCARD_DEF.UID_END)
+                        break;
+                }
+              
+                for (i = 3; i > -1; i--)
+                {
+                    strUID += Uid[i].ToString("X2");
+                }
+
+              //  strUID += "88";
+
+                KBsimulation(strUID, "{ENTER}");
+
+            }
+            catch (Exception ex)
+            {
+              
+            }
+        }
+
+        private void btn_CloseReader_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            timer1.Stop();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            readnfc2(0);
+        }
+
+        private void KBsimulation(string InputData, string inputkey)
+        {
+            if (this.WindowState == FormWindowState.Normal)
+            {
+                txtmsg.Text = InputData;
+            }
+            else
+            {
+                Clipboard.SetDataObject(InputData, true);
+                keybd_event(0x11, 0, 0, 0);
+                keybd_event(0x56, 0, 0, 0);
+                keybd_event(0x56, 0, 2, 0);
+                keybd_event(0x11, 0, 2, 0);
+
+                Thread.Sleep(50);
+
+                SendKeys.Send(inputkey);
+            }
+        }
+
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+                this.notifyIcon1.Visible = false;
+                this.ShowInTaskbar = true;
+            }
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+                this.notifyIcon1.Visible = true;
+            }
+        }
+    }
+}
